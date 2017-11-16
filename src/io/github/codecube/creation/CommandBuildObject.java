530a6ca69a5a02789e6fe5fd8a49b06515f09175
CodeCube <therealcodecube@gmail.com>
2017-11-15 18:07:31 -0800

    Created EditorWorld
    Moved code for managing the grid world editor thing into a singleton
    class.

--- src/io/github/codecube/creation/CommandBuildObject.java
+++ src/io/github/codecube/creation/CommandBuildObject.java
@@ -1,17 +1,10 @@
 package io.github.codecube.creation;
 
-import java.io.File;
-
-import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.World.Environment;
-import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import io.github.codecube.util.FileUtils;
 
 public class CommandBuildObject implements CommandExecutor {
 	public static final String OBJECT_EDITOR_WORLD_PREFIX = "objecteditor";
@@ -21,31 +14,18 @@ public class CommandBuildObject implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 
-			World world = Bukkit.getWorld(OBJECT_EDITOR_WORLD_PREFIX + "0");
-			if (world != null) {
-				System.out.println("Deleting world...");
-				if (!Bukkit.unloadWorld(world, true)) {
-					player.sendMessage("You must be outside the editor to use this command!");
-					return true;
-				}
-				File directory = world.getWorldFolder();
-				FileUtils.deleteFolder(directory);
+			World world = EditorWorld.getInstance().getWorld();
+			if (world == null) { // Something went wrong when recreating the world.
+				player.sendMessage("Could not reset editor! Are there people still in it?");
+				return false;
+			} else {
+				player.teleport(world.getSpawnLocation());
+				return true;
 			}
 
-			WorldCreator creator = new WorldCreator(OBJECT_EDITOR_WORLD_PREFIX + "0");
-			creator.environment(Environment.NORMAL);
-			creator.generateStructures(false);
-			creator.generator(new GridWorldGenerator());
-
-			world = creator.createWorld();
-			world.setMonsterSpawnLimit(0);
-			world.setAnimalSpawnLimit(0);
-
-			player.teleport(world.getSpawnLocation());
 		} else {
 			sender.sendMessage("You must be a player to use this command!");
 			return false;
 		}
-		return true;
 	}
 }
--- src/io/github/codecube/spigotfun/SpigotFunPlugin.java
+++ src/io/github/codecube/spigotfun/SpigotFunPlugin.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.codecube.creation.CommandBuildObject;
 import io.github.codecube.creation.CommandSaveObject;
+import io.github.codecube.creation.EditorWorld;
 
 public class SpigotFunPlugin extends JavaPlugin {
 	// Called when the plugin is first registered.
@@ -15,12 +16,11 @@ public class SpigotFunPlugin extends JavaPlugin {
 		// Register the test event listener.
 		getServer().getPluginManager().registerEvents(new TestListener(), this);
 
-		// Load the objecteditor world now, otherwise when it is created later, the code
-		// will not realize that there is already stuff in it and it will not be
-		// cleared.
-		getServer().getWorlds().add(CommandBuildObject.OBJECT_EDITOR_WORLD_PREFIX + "0");
-
 		// Ok, now for real non-testing stuff.
+		// Load the editor world now, otherwise when it is created later, the code will
+		// not realize that there is already stuff in it and it will not be cleared.
+		EditorWorld.getInstance().getWorld(); // Sets up the editor world.
+
 		// This command allows for creation of objects.
 		getCommand("buildobject").setExecutor(new CommandBuildObject());
 		// This command saves the object the user is editing to a file to be used later.
