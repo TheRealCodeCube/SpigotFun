1f8c21565bc3932d33e958acdacb99f8751ffd8f
CodeCube <therealcodecube@gmail.com>
2017-11-17 22:42:36 -0800

    Particle effects!
    There are now static functions for spawning particles, colorable
    particles, and lines of each in the Particles class.

--- src/io/github/codecube/spigotfun/CommandFunTest.java
+++ src/io/github/codecube/spigotfun/CommandFunTest.java
@@ -1,46 +1,26 @@
 package io.github.codecube.spigotfun;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
-import io.github.codecube.creation.HotbarToolbar;
-import io.github.codecube.creation.HotbarToolbarItem;
-import io.github.codecube.creation.HotbarToolbarItemListener;
-import io.github.codecube.util.StoneHoeIcons;
+import io.github.codecube.engine.Particles;
 
 public class CommandFunTest implements CommandExecutor {
 	// Called when someone uses the command.
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// Test out HotbarToolbar
-		if (sender instanceof Player) {
-			sender.sendMessage("Doing it!");
-			HotbarToolbarItem item1 = new HotbarToolbarItem(StoneHoeIcons.ICON_CLOCKWISE);
-			item1.setName("Rotate");
-			item1.setDescription(
-					"Rotates a thing by a certain rotation amount so that it becomes more rotated than it was before and yeah more words.");
-			HotbarToolbarItem item2 = new HotbarToolbarItem();
-			item1.setListener(new HotbarToolbarItemListener() {
-				@Override
-				public boolean onUse(HotbarToolbarItem used, Player user, Action action, boolean sneaking) {
-					user.sendMessage("You did it!");
-					return true;
-				}
-
-				@Override
-				public ItemStack onUpdate(HotbarToolbarItem used, Player holder, boolean sneaking) {
-					return null;
-				}
-			});
-			HotbarToolbar toolbar = new HotbarToolbar((Player) sender);
-			toolbar.addItem(item1, 0);
-			toolbar.addItem(item2, 5);
-			toolbar.update();
-		}
+		// Test out particle systems.
+		List<Player> players = new ArrayList<>();
+		players.add((Player) sender);
+		Particles.drawParticleLine(players, Particle.DRAGON_BREATH, players.get(0).getLocation(), new Vector(2, 2, 2),
+				0.1);
 
 		return true; // Return false if the command was used incorrectly.
 	}
